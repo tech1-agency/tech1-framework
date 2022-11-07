b5bf1cc6cd647923c65b65c79c1ee0c7822a5745
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2022-11-07 13:14:43 +0000

    start on parametrized tests on asserts layer

--- tech1-framework-domain/src/test/java/io/tech1/framework/domain/asserts/AssertsTests.java
+++ tech1-framework-domain/src/test/java/io/tech1/framework/domain/asserts/AssertsTests.java
@@ -1,5 +1,38 @@
 package io.tech1.framework.domain.asserts;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static io.tech1.framework.domain.asserts.Asserts.assertNonNullOrThrow;
+import static java.util.Objects.isNull;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+
 public class AssertsTests {
-    // ignored
+
+    private static Stream<Arguments> assertNonNullOrThrowTestParameters() {
+        return Stream.of(
+                Arguments.of(new Object(), null),
+                Arguments.of(null, "errorMessage")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("assertNonNullOrThrowTestParameters")
+    public void assertNonNullOrThrowTest(Object object, String expectedErrorMessage) {
+        // Act
+        var throwable = catchThrowable(() -> assertNonNullOrThrow(object, expectedErrorMessage));
+
+        // Assert
+        if (isNull(expectedErrorMessage)) {
+            assertThat(throwable).isNull();
+        } else {
+            assertThat(throwable).isNotNull();
+            assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+            assertThat(throwable).hasMessageContaining(expectedErrorMessage);
+        }
+    }
 }

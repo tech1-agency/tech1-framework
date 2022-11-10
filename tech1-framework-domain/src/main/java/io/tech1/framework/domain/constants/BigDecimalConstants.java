ed919eb36a1b9646eaa7a5f458980408a97bf876
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2022-11-10 16:09:33 +0000

    add ONE_HUNDRED to bigdecimal, biginteger constants

--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/constants/BigDecimalConstants.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/constants/BigDecimalConstants.java
@@ -7,4 +7,5 @@ import java.math.BigDecimal;
 @UtilityClass
 public class BigDecimalConstants {
     public static final BigDecimal MINUS_ONE = BigDecimal.valueOf(-1);
+    public static final BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100);
 }

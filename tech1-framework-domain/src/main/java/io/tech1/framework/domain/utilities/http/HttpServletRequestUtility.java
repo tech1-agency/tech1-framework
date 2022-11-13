2e284b2e99a73747916fbef9c354e8806f273917
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2022-11-13 14:12:17 +0000

    add UserAgentDetails and "browscap-java" maven dependency

--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/utilities/http/HttpServletRequestUtility.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/utilities/http/HttpServletRequestUtility.java
@@ -1,11 +1,19 @@
 package io.tech1.framework.domain.utilities.http;
 
+import com.blueconic.browscap.BrowsCapField;
+import com.blueconic.browscap.ParseException;
+import com.blueconic.browscap.UserAgentService;
+import io.tech1.framework.domain.http.requests.UserAgentDetails;
+import io.tech1.framework.domain.http.requests.UserAgentHeader;
 import lombok.experimental.UtilityClass;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
 
 import static io.tech1.framework.domain.utilities.exceptions.ExceptionsMessagesUtility.invalidAttribute;
 import static java.util.Objects.isNull;
+import static org.springframework.util.StringUtils.hasLength;
 
 @UtilityClass
 public class HttpServletRequestUtility {
@@ -20,5 +28,43 @@ public class HttpServletRequestUtility {
         }
         var queryString = request.getQueryString();
         return isNull(queryString) ? requestURL.toString() : requestURL.append("?").append(queryString).toString();
+    }
+
+    public static String getClientIpAddr(HttpServletRequest request) {
+        var unknown = "unknown";
+        var ip = request.getHeader("X-Forwarded-For");
+        if (!hasLength(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (!hasLength(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (!hasLength(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (!hasLength(ip) || unknown.equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (!hasLength(ip) || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+    public static UserAgentDetails getUserAgentDetails(HttpServletRequest request) {
+        try {
+            var userAgentParser = new UserAgentService().loadParser(
+                    List.of(
+                            BrowsCapField.BROWSER,
+                            BrowsCapField.PLATFORM,
+                            BrowsCapField.DEVICE_TYPE
+                    )
+            );
+            var userAgentHeader = new UserAgentHeader(request);
+            var capabilities = userAgentParser.parse(userAgentHeader.getValue());
+            return UserAgentDetails.processed(capabilities);
+        } catch (IOException | ParseException ex) {
+            return UserAgentDetails.unknown(ex.getMessage());
+        }
     }
 }

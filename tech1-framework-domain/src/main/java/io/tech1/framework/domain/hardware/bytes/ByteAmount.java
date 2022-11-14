a45c617d600e0fee39ae1e9d7bba81bd0b84e060
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2022-11-14 13:48:30 +0000

    start on hardware classes: byteunit, bytesize, byteamount

---
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/hardware/bytes/ByteAmount.java
@@ -0,0 +1,48 @@
+package io.tech1.framework.domain.hardware.bytes;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+import static io.tech1.framework.domain.asserts.Asserts.assertNonNullOrThrow;
+import static io.tech1.framework.domain.utilities.exceptions.ExceptionsMessagesUtility.invalidAttribute;
+
+// JSON
+// NOT used in serialization/deserialization
+// Lombok
+@Getter
+@EqualsAndHashCode
+@ToString
+public class ByteAmount {
+    private final BigDecimal amount;
+    private final ByteUnit unit;
+
+    public ByteAmount(
+            long bytes,
+            ByteUnit unit
+    ) {
+        assertNonNullOrThrow(unit, invalidAttribute("ByteAmount.unit"));
+        this.amount = new ByteSize(bytes).getBy(unit);
+        this.unit = unit;
+    }
+
+    public static ByteAmount ofGb(
+            long bytes
+    ) {
+        return new ByteAmount(
+                bytes,
+                ByteUnit.GIGABYTE
+        );
+    }
+
+    public static ByteAmount ofMB(
+            long bytes
+    ) {
+        return new ByteAmount(
+                bytes,
+                ByteUnit.MEGABYTE
+        );
+    }
+}

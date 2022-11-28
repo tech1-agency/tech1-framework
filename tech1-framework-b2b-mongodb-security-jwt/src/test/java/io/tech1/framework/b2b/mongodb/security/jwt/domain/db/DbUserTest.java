a68e3ba5ef2c1a4239acdb58f2f83b99931b0907
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2022-11-28 12:02:40 +0200

    complete tests: dbUser, userSessionsTable

---
+++ tech1-framework-b2b-mongodb-security-jwt/src/test/java/io/tech1/framework/b2b/mongodb/security/jwt/domain/db/DbUserTest.java
@@ -0,0 +1,54 @@
+package io.tech1.framework.b2b.mongodb.security.jwt.domain.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+import static io.tech1.framework.domain.utilities.random.RandomUtility.*;
+import static io.tech1.framework.domain.utilities.reflections.ReflectionUtility.setPrivateField;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DbUserTest {
+
+    @Test
+    public void getNotNullAttributesTest() {
+        // Arrange
+        var user = new DbUser(
+                randomUsername(),
+                randomPassword().getValue(),
+                randomZoneId().getId(),
+                List.of(
+                        new SimpleGrantedAuthority("admin123")
+                )
+        );
+
+        // Act
+        var actual = user.getNotNullAttributes();
+
+        // Assert
+        assertThat(actual).isNotNull();
+        assertThat(actual).hasSize(0);
+    }
+
+    @Test
+    public void getNotNullAttributesLegacyMigrationNullPointerExceptionTest() throws NoSuchFieldException, IllegalAccessException {
+        // Arrange
+        var user = new DbUser(
+                randomUsername(),
+                randomPassword().getValue(),
+                randomZoneId().getId(),
+                List.of(
+                        new SimpleGrantedAuthority("admin123")
+                )
+        );
+        setPrivateField(user, "attributes", null);
+
+        // Act
+        var actual = user.getNotNullAttributes();
+
+        // Assert
+        assertThat(actual).isNotNull();
+        assertThat(actual).hasSize(0);
+    }
+}

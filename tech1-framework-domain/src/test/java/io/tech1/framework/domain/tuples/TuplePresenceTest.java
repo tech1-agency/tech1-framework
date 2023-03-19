f2a31d4e936eeb221d515d3c6737a6560fd9ebe0
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2023-03-19 15:10:11 +0200

    add tuple presence (present, value)

---
+++ tech1-framework-domain/src/test/java/io/tech1/framework/domain/tuples/TuplePresenceTest.java
@@ -0,0 +1,55 @@
+package io.tech1.framework.domain.tuples;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import io.tech1.framework.domain.tests.runners.AbstractFolderSerializationRunner;
+import lombok.SneakyThrows;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static io.tech1.framework.domain.tests.io.TestsIOUtils.readFile;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TuplePresenceTest extends AbstractFolderSerializationRunner {
+
+    private static Stream<Arguments> serializeTest() {
+        return Stream.of(
+                Arguments.of(TuplePresence.present("PRESENT"), "tuple-presence-present.json"),
+                Arguments.of(TuplePresence.absent("ABSENT"), "tuple-presence-absent.json")
+        );
+    }
+
+    @Override
+    protected String getFolder() {
+        return "tuples";
+    }
+
+    @ParameterizedTest
+    @MethodSource("serializeTest")
+    public void serialize(TuplePresence<String> tuplePresence, String fileName) {
+        // Act
+        var json = this.writeValueAsString(tuplePresence);
+
+        // Assert
+        assertThat(json).isNotNull();
+        assertThat(json).isEqualTo(readFile(this.getFolder(), fileName));
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @MethodSource("serializeTest")
+    public void deserializeTest(TuplePresence<String> tuplePresence, String fileName) {
+        // Arrange
+        var json = readFile(this.getFolder(), fileName);
+        var typeReference = new TypeReference<TuplePresence<String>>() {};
+
+        // Act
+        var tuple = OBJECT_MAPPER.readValue(json, typeReference);
+
+        // Assert
+        assertThat(tuple).isNotNull();
+        assertThat(tuple).isEqualTo(tuplePresence);
+    }
+}

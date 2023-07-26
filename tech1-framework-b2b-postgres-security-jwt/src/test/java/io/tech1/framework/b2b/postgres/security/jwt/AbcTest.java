6441344f9b1d755cd7d2405dd3bab02b879e852e
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2023-07-26 14:00:19 +0300

    add b2b-postgres-security-jwt module skeleton

---
+++ tech1-framework-b2b-postgres-security-jwt/src/test/java/io/tech1/framework/b2b/postgres/security/jwt/AbcTest.java
@@ -0,0 +1,17 @@
+package io.tech1.framework.b2b.postgres.security.jwt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AbcTest {
+
+    @Test
+    void testTest() {
+        // Arrange
+        var actual = 0;
+
+        // Assert
+        assertThat(actual).isNotNull();
+    }
+}

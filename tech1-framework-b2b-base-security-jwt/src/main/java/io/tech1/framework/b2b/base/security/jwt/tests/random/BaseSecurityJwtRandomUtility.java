39bcc196f0d1336f9d3f9db52c55f0a2814ec9af
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2023-08-01 15:09:46 +0300

    authentication flow: add access token on base jwt utility

--- tech1-framework-b2b-base-security-jwt/src/main/java/io/tech1/framework/b2b/base/security/jwt/tests/random/BaseSecurityJwtRandomUtility.java
+++ tech1-framework-b2b-base-security-jwt/src/main/java/io/tech1/framework/b2b/base/security/jwt/tests/random/BaseSecurityJwtRandomUtility.java
@@ -1,14 +1,21 @@
 package io.tech1.framework.b2b.base.security.jwt.tests.random;
 
 import io.jsonwebtoken.impl.DefaultClaims;
+import io.tech1.framework.domain.properties.base.TimeAmount;
+import io.tech1.framework.domain.utilities.time.TimestampUtility;
 import lombok.experimental.UtilityClass;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static io.tech1.framework.b2b.base.security.jwt.domain.jwt.JwtTokenValidatedClaims.getIssuedAt;
 import static io.tech1.framework.domain.tests.constants.TestsUsernamesConstants.TECH1;
+import static io.tech1.framework.domain.utilities.time.DateUtility.convertLocalDateTime;
+import static java.time.ZoneOffset.UTC;
 
 @UtilityClass
 public class BaseSecurityJwtRandomUtility {
@@ -20,8 +27,22 @@ public class BaseSecurityJwtRandomUtility {
     public static DefaultClaims validDefaultClaims() {
         var claims = new DefaultClaims();
         claims.setSubject(TECH1.identifier());
+        var timeAmount = TimeAmount.of(1, ChronoUnit.HOURS);
+        var expiration = convertLocalDateTime(LocalDateTime.now(UTC).plus(timeAmount.getAmount(), timeAmount.getUnit()), UTC);
         claims.setIssuedAt(getIssuedAt());
-        claims.setExpiration(getIssuedAt());
+        claims.setExpiration(expiration);
+        claims.put("authorities", authorities("admin", "user"));
+        return claims;
+    }
+
+    public static DefaultClaims expiredDefaultClaims() {
+        var claims = new DefaultClaims();
+        claims.setSubject(TECH1.identifier());
+        var currentTimestamp = TimestampUtility.getCurrentTimestamp();
+        var issuedAt = new Date(currentTimestamp);
+        var expiration = new Date(currentTimestamp - 1000);
+        claims.setIssuedAt(issuedAt);
+        claims.setExpiration(expiration);
         claims.put("authorities", authorities("admin", "user"));
         return claims;
     }

600a14a7ab66d56261569442af0d6dae0dc3dfe1
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2023-09-26 20:13:51 +0300

    add renew manually integration way in Postman

--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/http/requests/IPAddress.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/http/requests/IPAddress.java
@@ -1,9 +1,11 @@
 package io.tech1.framework.domain.http.requests;
 
+import org.jetbrains.annotations.NotNull;
+
 import static io.tech1.framework.domain.utilities.random.RandomUtility.localhost;
 import static java.util.Objects.nonNull;
 
-public record IPAddress(String value) {
+public record IPAddress(@NotNull String value) {
 
     public IPAddress(String value) {
         this.value = nonNull(value) ? value : localhost().value();

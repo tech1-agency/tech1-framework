7448cd7717f775dd3f800341418512e56a8eaf25
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-01-25 19:22:29 +0200

    SNAPSHOT: v2.1

--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/constants/FrameworkConstants.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/constants/FrameworkConstants.java
@@ -4,5 +4,5 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class FrameworkConstants {
-    public static final String VERSION_RUNTIME = "tech1-framework v2.0";
+    public static final String VERSION_RUNTIME = "tech1-framework v2.1-SNAPSHOT";
 }

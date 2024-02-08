9ae48cb7f04451bdcede9887167fe8c350761bf2
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-02-08 18:54:51 +0200

    -

--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/utilities/printer/PRINTER.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/utilities/printer/PRINTER.java
@@ -9,6 +9,70 @@ import lombok.extern.slf4j.Slf4j;
 public class PRINTER {
 
     // =================================================================================================================
+    // PRINT
+    // =================================================================================================================
+    private static void print(String message) {
+        var printerLevel = SystemProperties.getPrinterLevel();
+        if (printerLevel.isError()) {
+            error(message);
+        } else if (printerLevel.isWarn()) {
+            warn(message);
+        } else if (printerLevel.isInfo()) {
+            info(message);
+        } else {
+            debug(message);
+        }
+    }
+
+    private static void print(String format, Object arg) {
+        var printerLevel = SystemProperties.getPrinterLevel();
+        if (printerLevel.isError()) {
+            error(format, arg);
+        } else if (printerLevel.isWarn()) {
+            warn(format, arg);
+        } else if (printerLevel.isInfo()) {
+            info(format, arg);
+        } else {
+            debug(format, arg);
+        }
+    }
+
+    private static void print(String format, Object... arguments) {
+        var printerLevel = SystemProperties.getPrinterLevel();
+        if (printerLevel.isError()) {
+            error(format, arguments);
+        } else if (printerLevel.isWarn()) {
+            warn(format, arguments);
+        } else if (printerLevel.isInfo()) {
+            info(format, arguments);
+        } else {
+            debug(format, arguments);
+        }
+    }
+
+    // =================================================================================================================
+    // DEBUG
+    // =================================================================================================================
+
+    private static void debug(String message) {
+        if (SystemProperties.isPrinterEnabled()) {
+            LOGGER.debug(message);
+        }
+    }
+
+    private static void debug(String format, Object arg) {
+        if (SystemProperties.isPrinterEnabled()) {
+            LOGGER.debug(format, arg);
+        }
+    }
+
+    private static void debug(String format, Object... arguments) {
+        if (SystemProperties.isPrinterEnabled()) {
+            LOGGER.debug(format, arguments);
+        }
+    }
+
+    // =================================================================================================================
     // INFO
     // =================================================================================================================
 
@@ -25,6 +89,28 @@ public class PRINTER {
     }
 
     public static void info(String format, Object... arguments) {
+        if (SystemProperties.isPrinterEnabled()) {
+            LOGGER.info(format, arguments);
+        }
+    }
+
+    // =================================================================================================================
+    // INFO
+    // =================================================================================================================
+
+    private static void warn(String message) {
+        if (SystemProperties.isPrinterEnabled()) {
+            LOGGER.info(message);
+        }
+    }
+
+    private static void warn(String format, Object arg) {
+        if (SystemProperties.isPrinterEnabled()) {
+            LOGGER.info(format, arg);
+        }
+    }
+
+    private static void warn(String format, Object... arguments) {
         if (SystemProperties.isPrinterEnabled()) {
             LOGGER.info(format, arguments);
         }
--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/utilities/system/SystemProperties.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/utilities/system/SystemProperties.java
@@ -1,12 +1,18 @@
 package io.tech1.framework.domain.utilities.system;
 
 import io.tech1.framework.domain.constants.SystemPropertiesConstants;
+import io.tech1.framework.domain.enums.PrinterLevel;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class SystemProperties {
+    private static final PrinterLevel PRINTER = PrinterLevel.findOrDebug(System.getProperty(SystemPropertiesConstants.TECH1_FRAMEWORK_PRINTER));
+
+    public static PrinterLevel getPrinterLevel() {
+        return PRINTER;
+    }
 
     public static boolean isPrinterEnabled() {
-        return "true".equals(System.getProperty(SystemPropertiesConstants.TECH1_FRAMEWORK_PRINTER_DEBUG));
+        return "true".equals(System.getProperty(SystemPropertiesConstants.TECH1_FRAMEWORK_PRINTER));
     }
 }

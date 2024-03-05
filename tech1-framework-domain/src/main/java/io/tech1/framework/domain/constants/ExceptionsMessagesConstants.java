8f3ce5eb374e16a05566fa1cbcf6b9bab0e99d4e
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-03-05 14:41:01 +0000

    -

--- tech1-framework-domain/src/main/java/io/tech1/framework/domain/constants/ExceptionsMessagesConstants.java
+++ tech1-framework-domain/src/main/java/io/tech1/framework/domain/constants/ExceptionsMessagesConstants.java
@@ -7,6 +7,7 @@ public class ExceptionsMessagesConstants {
     public static final String CONTACT_DEVELOPMENT_TEAM = "%s. Please contact development team";
 
     public static final String INVALID_ATTRIBUTE = "Attribute `%s` is invalid";
+    // TODO [YYL] Missing values -> Missing value
     public static final String INVALID_ATTRIBUTE_REQUIRED_MISSING_VALUES = "Attribute `%s` is invalid. Required values: `[%s]`. Missing values: `[%s]`";
 
     public static final String ENTITY_NOT_FOUND = "%s: Not Found, id = %s";

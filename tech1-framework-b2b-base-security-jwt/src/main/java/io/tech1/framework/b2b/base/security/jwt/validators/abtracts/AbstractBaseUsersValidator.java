b4f2a60394d6bbc1ec53f574808241a55f129e23
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-04-05 14:47:55 +0300

    cleanup

--- tech1-framework-b2b-base-security-jwt/src/main/java/io/tech1/framework/b2b/base/security/jwt/validators/abtracts/AbstractBaseUsersValidator.java
+++ tech1-framework-b2b-base-security-jwt/src/main/java/io/tech1/framework/b2b/base/security/jwt/validators/abtracts/AbstractBaseUsersValidator.java
@@ -8,6 +8,7 @@ import io.tech1.framework.domain.base.Username;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 
+import static io.tech1.framework.domain.utilities.exceptions.ExceptionsMessagesUtility.entityAlreadyUsed;
 import static java.util.Objects.nonNull;
 
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,7 +20,7 @@ public abstract class AbstractBaseUsersValidator implements BaseUsersValidator {
     public void validateUserUpdateRequest1(Username username, RequestUserUpdate1 request) {
         var user = this.usersRepository.findByEmailAsJwtUserOrNull(request.email());
         if (nonNull(user) && !user.username().equals(username)) {
-            throw new IllegalArgumentException("Email is already used");
+            throw new IllegalArgumentException(entityAlreadyUsed("Email", request.email().value()));
         }
     }
 

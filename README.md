96d5f28c6fe36e06dc23b14dd09d7aab78fd4b8a
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-04-26 15:33:19 +0300

    — Modification: JWT token configs secret key regeneration

--- README.md
+++ README.md
@@ -17,4 +17,4 @@ Last deployed version: **1.16**
 `mvn clean verify` when you want to be sure, that whole project just works  
 
 ### Release Notes [Development v2.8.1]
-— TBD
+— Modification: JWT token configs secret key regeneration
--- tech1-framework-b2b-postgres-server/run.sh
+++ tech1-framework-b2b-postgres-server/run.sh
@@ -2,7 +2,7 @@
 
 METHOD=maven
 PORT=8484
-SPRING_BOOT_PROFILE=dev
+SPRING_BOOT_PROFILE=
 SPRING_BOOT_CONFIG_LOCATION=classpath:application.yml,classpath:application-dev.yml
 JASYPT_PASSWORD=JJEPTECH1
 JVM_ARGUMENTS="-Xms512m -Xmx2g --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.math=ALL-UNNAMED"
@@ -18,4 +18,4 @@ echo "==========================================================================
 echo "PostgreSQL init [Completed]"
 echo "================================================================================================================="
 
-java-run-spring-boot-dev-profile-v4.sh $METHOD $PORT $SPRING_BOOT_PROFILE $SPRING_BOOT_CONFIG_LOCATION $JASYPT_PASSWORD "$JVM_ARGUMENTS"
+java-run-spring-boot-dev-profile-v4.sh $METHOD $PORT "$SPRING_BOOT_PROFILE" $SPRING_BOOT_CONFIG_LOCATION $JASYPT_PASSWORD "$JVM_ARGUMENTS"

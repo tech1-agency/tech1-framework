4efc6c30e5659fa098182b3eab90c1ce98e2a260
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-07-08 18:03:40 +0300

    next releases/snapshots scripts [2]

--- next_snapshot.sh
+++ next_snapshot.sh
@@ -23,7 +23,13 @@ echo "==========================================================================
 echo "================================================================================================================="
 echo "$PREFIX GitHub Action, DOCKER_VERSION started"
 
-#?
+MAJOR_VERSION_NUMBER=$(grep "DOCKER_VERSION:" "$FILE_PATH" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | awk -F '[.-]' '{print $1}')
+MINOR_VERSION_NUMBER=$(grep "DOCKER_VERSION:" "$FILE_PATH" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | awk -F '[.-]' '{print $2}')
+PATCH_VERSION_NUMBER=$(grep "DOCKER_VERSION:" "$FILE_PATH" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | awk -F '[.-]' '{print $3}')
+((PATCH_VERSION_NUMBER++))
+DOCKER_VERSION_NEXT_SNAPSHOT="$MAJOR_VERSION_NUMBER.$MINOR_VERSION_NUMBER.$PATCH_VERSION_NUMBER-SNAPSHOT"
+
+sed -i '' "s/DOCKER_VERSION: .*/DOCKER_VERSION: '$DOCKER_VERSION_NEXT_SNAPSHOT'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
 
 echo "$PREFIX GitHub Action, DOCKER_VERSION has been completed"
 echo "================================================================================================================="
---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PREFIX="[NextSnapshot]"
+FILE_PATH=".github/workflows/main.yml"
+GITHUB_ACTION_MAIN_WORKFLOW=".github/workflows/main.yml"
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, DOCKER_VERSION started"
+
+MAJOR_VERSION_NUMBER=$(grep "DOCKER_VERSION:" "$FILE_PATH" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | awk -F '[.-]' '{print $1}')
+MINOR_VERSION_NUMBER=$(grep "DOCKER_VERSION:" "$FILE_PATH" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | awk -F '[.-]' '{print $2}')
+PATCH_VERSION_NUMBER=$(grep "DOCKER_VERSION:" "$FILE_PATH" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+' | awk -F '[.-]' '{print $3}')
+((PATCH_VERSION_NUMBER++))
+DOCKER_VERSION_NEXT_SNAPSHOT="$MAJOR_VERSION_NUMBER.$MINOR_VERSION_NUMBER.$PATCH_VERSION_NUMBER-SNAPSHOT"
+
+sed -i '' "s/DOCKER_VERSION: .*/DOCKER_VERSION: '$DOCKER_VERSION_NEXT_SNAPSHOT'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
+
+echo "$PREFIX GitHub Action, DOCKER_VERSION has been completed"
+echo "================================================================================================================="
+

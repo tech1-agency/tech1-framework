127ba1f31acb0f1c8b4d051287831ffaeb9b6628
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-07-08 17:50:46 +0300

    next releases/snapshots scripts [1]

--- next_release.sh
+++ next_release.sh
@@ -1,3 +1,36 @@
 #!/usr/bin/env bash
 
+PREFIX="[NextRelease]"
+GITHUB_ACTION_MAIN_WORKFLOW=".github/workflows/main.yml"
+
+echo "================================================================================================================="
+echo "$PREFIX Maven versions started"
+
 mvn versions:set -DremoveSnapshot -DgenerateBackupPoms=false
+
+echo "$PREFIX Maven versions has been completed"
+echo "================================================================================================================="
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, MAVEN_DEPLOYMENT_ENABLED started"
+
+sed -i '' "s/MAVEN_DEPLOYMENT_ENABLED: .*/MAVEN_DEPLOYMENT_ENABLED: 'true'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
+
+echo "$PREFIX GitHub Action, MAVEN_DEPLOYMENT_ENABLED has been completed"
+echo "================================================================================================================="
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, DOCKER_VERSION started"
+
+sed -i '' "s/DOCKER_VERSION: '\(.*\)-SNAPSHOT'/DOCKER_VERSION: '\1'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
+
+echo "$PREFIX GitHub Action, DOCKER_VERSION has been completed"
+echo "================================================================================================================="
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, DOCKER_PUSH_ENABLED started"
+
+sed -i '' "s/DOCKER_PUSH_ENABLED: .*/DOCKER_PUSH_ENABLED: 'true'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
+
+echo "$PREFIX GitHub Action, DOCKER_PUSH_ENABLED has been completed"
+echo "================================================================================================================="
--- next_snapshot.sh
+++ next_snapshot.sh
@@ -1,3 +1,37 @@
 #!/usr/bin/env bash
 
+PREFIX="[NextSnapshot]"
+GITHUB_ACTION_MAIN_WORKFLOW=".github/workflows/main.yml"
+
+
+echo "================================================================================================================="
+echo "$PREFIX Maven versions started"
+
 mvn versions:set -DnextSnapshot=true -DgenerateBackupPoms=false
+
+echo "$PREFIX Maven versions has been completed"
+echo "================================================================================================================="
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, MAVEN_DEPLOYMENT_ENABLED started"
+
+sed -i '' "s/MAVEN_DEPLOYMENT_ENABLED: .*/MAVEN_DEPLOYMENT_ENABLED: 'false'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
+
+echo "$PREFIX GitHub Action, MAVEN_DEPLOYMENT_ENABLED has been completed"
+echo "================================================================================================================="
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, DOCKER_VERSION started"
+
+#?
+
+echo "$PREFIX GitHub Action, DOCKER_VERSION has been completed"
+echo "================================================================================================================="
+
+echo "================================================================================================================="
+echo "$PREFIX GitHub Action, DOCKER_PUSH_ENABLED started"
+
+sed -i '' "s/DOCKER_PUSH_ENABLED: .*/DOCKER_PUSH_ENABLED: 'false'/" "$GITHUB_ACTION_MAIN_WORKFLOW"
+
+echo "$PREFIX GitHub Action, DOCKER_PUSH_ENABLED has been completed"
+echo "================================================================================================================="

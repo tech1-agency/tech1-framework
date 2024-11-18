c2527809a88a7b499e0fd92a48af9b4d3d598c22
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2024-11-18 17:33:17 +0200

    jbst migration #47

--- jbst-foundation/src/main/java/jbst/foundation/domain/base/AbstractAuthority.java
+++ jbst-foundation/src/main/java/jbst/foundation/domain/base/AbstractAuthority.java
@@ -4,6 +4,7 @@ public interface AbstractAuthority {
     String SUPERADMIN = "superadmin";
     String INVITATION_CODE_READ = "invitationCode:read";
     String INVITATION_CODE_WRITE = "invitationCode:write";
+    String PROMETHEUS_READ = "prometheus:read";
 
     String getValue();
 }
